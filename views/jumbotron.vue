9953f0ad708e019f3847089bb0f68c757adb7b66
guruprasaad123 <jiiva116@live.com>
2017-12-09 20:30:56 +0500

    initial

---
+++ views/jumbotron.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="jumbotron " class="jumbotron text-center">
+  <div class="container">
+  <h1 >{{header}}</h1>
+  <p class="lead"><slot></slot></p>
+  <div class="col" >
+
+ </div>
+  </div>
+  
+</div>
+
+</template>
+
+<script>
+
+module.exports = {
+    props: {
+      header:{required:true},
+
+    },
+    data(){
+      return{
+
+      };
+    }
+
+}
+
+</script>
+<style >
+#jumbotron{
+  display:block;
+
+}
+</style>
